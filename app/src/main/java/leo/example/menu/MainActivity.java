531aea78d85ae3f058c8d628af7d0cfa2b33d2cd
leokraken <joleocl@gmail.com>
2017-01-07 18:42:15 -0300

    Add resources

--- app/src/main/java/leo/example/menu/MainActivity.java
+++ app/src/main/java/leo/example/menu/MainActivity.java
@@ -1,6 +1,7 @@
 package leo.example.menu;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -8,6 +9,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String package_name = getApplication().getPackageName();
+        Resources resources = getApplication().getResources();
+        setContentView(resources.getIdentifier("activity_main", "layout", package_name));
         setContentView(R.layout.activity_main);
     }
 }
