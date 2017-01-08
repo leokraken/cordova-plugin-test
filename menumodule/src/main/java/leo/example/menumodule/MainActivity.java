7844172411cab0a4fa855843118ba5bfb838f433
leokraken <joleocl@gmail.com>
2017-01-08 00:22:59 -0300

    Create simple module with intents

--- menumodule/src/main/java/leo/example/menumodule/MainActivity.java
+++ menumodule/src/main/java/leo/example/menumodule/MainActivity.java
@@ -1,14 +1,15 @@
-package leo.example.menu;
+package leo.example.menumodule;
 
 import android.app.Activity;
-import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("DEBUGSDSD", "THEKSDKJJDKJKDKJD");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
     }
 }
